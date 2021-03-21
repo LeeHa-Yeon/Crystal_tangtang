7ce8f0205e364cd96414b5aef8c4024b26d16adc
Lee Ha Yeon <gkdus1613@icloud.com>
2021-03-21 22:24:03 +0900

    내 정보창 화면 구현

---
+++ Crystal_tangtang/Tab/UserInfo/UserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserViewController.swift
+//  Crystal_tangtang
+//
+//  Created by 이하연 on 2021/03/21.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
